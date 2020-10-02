a11b901309c8f6435efd6a7db03045d64e57a06b
arq-gabo <jgabrielguerral@gmail.com>
2020-10-01 21:31:53 -0500

    [FEATURE]Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,30 @@ export default class extends React.Component {
       <div>
         <h1>Hola Mundo!!!</h1>
         <p>Bienvenidos</p>
+
+        <img src="platzi-logo-small.png" alt="Logo" />
+
+        <style jsx>{`
+          h1 {
+            color: red;
+          }
+          :global(p) {
+            color: green;
+          }
+          img {
+            max-width: 50%;
+            display: block;
+            margin: 0 auto;
+          }
+        `}</style>
+
+        <style jsx global>
+          {`
+            body {
+              background: white;
+            }
+          `}
+        </style>
       </div>
     );
   }
