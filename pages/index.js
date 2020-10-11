24217e02648f434f589ed491ad50daa63978ffab
arq-gabo <jgabrielguerral@gmail.com>
2020-10-11 16:12:56 -0500

    [FEATURE]Creando componentes en React

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import "isomorphic-fetch";
 import Link from "next/link";
+import Layout from "../components/Layout";
+import ChannelGrid from "../components/ChannelGrid";
 
 export default class extends React.Component {
   static async getInitialProps() {
@@ -13,65 +15,9 @@ export default class extends React.Component {
     const { channels } = this.props;
 
     return (
-      <div>
-        <header>Podcast</header>
-
-        <div className="channels">
-          {channels.map((channel) => (
-            <Link href={`/channel?id=${channel.id}`} prefetch>
-              <a className="channel">
-                <img src={channel.urls.logo_image.original} alt="" />
-                <h2>{channel.title}</h2>
-              </a>
-            </Link>
-          ))}
-        </div>
-
-        <style jsx>{`
-          header {
-            color: #fff;
-            background: #8756ca;
-            padding: 15px;
-            text-align: center;
-          }
-
-          .channels {
-            display: grid;
-            grid-gap: 15px;
-            padding: 15px;
-            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-          }
-
-          .channel {
-            display: block;
-            border-radius: 3px;
-            box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
-            margin-botton: 0.5em;
-          }
-
-          .channel img {
-            width: 100%;
-          }
-
-          h1 {
-            padding: 5px;
-            font-size: 0.9em;
-            font-weight: 600;
-            margin: 0;
-            text-align: center;
-          }
-        `}</style>
-
-        <style jsx global>
-          {`
-            body {
-              margin: 0;
-              font-family: system-ui;
-              background: white;
-            }
-          `}
-        </style>
-      </div>
+      <Layout title="App de Gabo">
+        <ChannelGrid channels={channels} />
+      </Layout>
     );
   }
 }
