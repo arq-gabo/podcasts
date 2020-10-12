f3cb9b8b7b0d24db97fc7eaa08b0b940b8303064
arq-gabo <jgabrielguerral@gmail.com>
2020-10-12 15:26:27 -0500

    [FEATURE]Estilando Paginas creando componentes

---
+++ components/Banner.js
@@ -0,0 +1,26 @@
+export default class Banner extends React.Component {
+  render() {
+    const { channel } = this.props;
+
+    return (
+      <div>
+        <div
+          className="banner"
+          style={{
+            backgroundImage: `url(${channel.urls.banner_image.original})`,
+          }}
+        />
+
+        <style jsx>{`
+          .banner {
+            width: 100%;
+            padding-bottom: 25%;
+            background-position: 50% 50%;
+            background-size: cover;
+            background-color: #aaa;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
